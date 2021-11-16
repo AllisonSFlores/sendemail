4038f9e1712d3a286775cd793740040698771e7d
cabadilla <54191657+cabadilla@users.noreply.github.com>
2021-11-16 11:46:07 -0600

    revertir commit

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ const upload = multer({ storage: storage });
 app.post('/', upload.single('file'), (req, res) => {});
 
 function createHtml(correo, pass) {
-    var texto = "<html><body><div><img src=" + "\"https://drive.google.com/uc?export=view&id=1Ab0xeq9NwHg78vk735gNMVtN1QEBrAtA\"" +
-        ">" + "<p></p>" + "Bienvenido Tu correo es: " + correo + "<p></p>" + "Tu contrasena es: " + pass + "</div>";
+    var texto = "<html><body><div><img src=" + "\"https://drive.google.com/uc?export=view&id=1Ab0xeq9NwHg78vk735gNMVtN1QEBrAtA\"" + "<p></p>" +
+        ">" + "Bienvenido Tu correo es: " + correo + "<p></p>" + "Tu contrasena es: " + pass + "</div>";
     fs.writeFile('./documents/usuario.html', texto, error => {
         if (error)
             console.log(error);
@@ -41,16 +41,15 @@ function createHtml(correo, pass) {
 
 app.post("/sendUser", [
     (req, res) => {
-        //createHtml(req.body.email, req.body.pass);
-        var correo = req.body.email;
-        var pass = req.body.pass;
+        createHtml(req.body.email, req.body.pass);
         var mailOptions = {
             from: "Teachable <teachableap@gmail.com>",
             to: req.body.email,
             subject: "Nuevo usuario",
-            text: "Gracias por registrarse",
-            html: `<html><body><div><img src="https://drive.google.com/uc?export=view&id=1Ab0xeq9NwHg78vk735gNMVtN1QEBrAtA"
-            ><p></p>Bienvenido Tu correo es: ${correo} <p></p>Tu contrasena es: ${pass}</div>`,
+            text: "Grcias por registrarse",
+            attachments: [{
+                path: './documents/usuario.html'
+            }]
         }
         transporter.transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
@@ -93,5 +92,5 @@ app.post("/send", [
     }
 ]);
 
-
-app.listen(3000, () => console.log("SERVIDOR EN PUERTO 3000"))
+app.set('port',process.env.PORT || 3000)
+app.listen(app.get('port'), () => console.log("SERVIDOR EN PUERTO 3000"))
